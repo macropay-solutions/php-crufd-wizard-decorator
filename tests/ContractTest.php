a8134783404c4f69cbfd4892dff7824318a4d83d
Pantea Marius-ciclistu <macropay-solutions@users.noreply.github.com>
2026-09-17 13:14:27 +0300

    Fix unit test

--- tests/ContractTest.php
+++ tests/ContractTest.php
@@ -57,12 +57,16 @@ class ContractTest extends TestCase
         }, '__invoke']);
 
         $app->bind('events', [function () {
-            return new class () {
-                public function dispatch($event, $payload = [], $halt = false) { return null; }
+            return new class () implements \MacropaySolutions\Kernel\Contracts\Events\Dispatcher {
                 public function listen($events, $listener = null) {}
-                public function forget($event) {}
-                public function until($event, $payload = []) { return null; }
-                public function __call($name, $arguments) { return null; }
+                public function hasListeners(string $eventName): bool { return false; }
+                public function subscribe(object|string $subscriber) {}
+                public function until(string|object $event, mixed $payload = []) { return null; }
+                public function dispatch($event, $payload = [], $halt = false) { return null; }
+                public function push(string $event, array $payload = []) {}
+                public function flush(string $event) {}
+                public function forget(string $event) {}
+                public function forgetPushed() {}
             };
         }, '__invoke']);
 
